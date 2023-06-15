8c1150f2ca335435fbf1c7f10bd657f76488459c
Kornel Kaczmarek <avcio2q@gmail.com>
2023-06-15 16:52:38 +0200

    fixed a bug causing hidden images

--- src/js/script.js
+++ src/js/script.js
@@ -99,8 +99,7 @@ const select = {
   
         /* find active product (product that has active class) */
 
-        const activeProduct = document.querySelector('.active');
-  
+        const activeProduct = document.querySelector('.active:not(img)');
         /* if there is active product and it's not thisProduct.element, remove class active from it */
 
         if (activeProduct && activeProduct != thisProduct.element) {
