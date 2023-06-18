216c0720fadec42e105864ce8567a411cf0de7ce
Kornel Kaczmarek <avcio2q@gmail.com>
2023-06-18 16:08:24 +0200

    fixed a bug with folding cart when revealing new product

--- src/js/script.js
+++ src/js/script.js
@@ -138,9 +138,10 @@
 
         /* find active product (product that has active class) */
 
-        const activeProduct = document.querySelector('.active:not(img)');
+        const activeProducts = document.querySelectorAll('.active:not(img):not(.cart)');
         /* if there is active product and it's not thisProduct.element, remove class active from it */
 
+        for (const activeProduct of activeProducts )
         if (activeProduct && activeProduct != thisProduct.element) {
           activeProduct.classList.remove('active');
         }
