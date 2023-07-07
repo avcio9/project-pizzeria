f10fe0cfa2a933c74090e40428ac400031ad8942
Kornel Kaczmarek <avcio2q@gmail.com>
2023-07-07 23:05:26 +0200

    added some booking functionalities

--- src/js/settings.js
+++ src/js/settings.js
@@ -43,6 +43,7 @@ export const select = {
     peopleAmount: '.people-amount',
     hoursAmount: '.hours-amount',
     tables: '.floor-plan .table',
+    tablesWrapper: '.floor-plan',
   },
   nav: {
     links: '.main-nav a',
@@ -79,6 +80,7 @@ export const classNames = {
   booking: {
     loading: 'loading',
     tableBooked: 'booked',
+    selected: 'selected',
   },
   nav: {
     active: 'active',
@@ -112,8 +114,8 @@ export const settings = {
     url: '//localhost:3131',
     products: 'products',
     orders: 'orders',
-    booking: 'booking',
-    event: 'event',
+    booking: 'bookings',
+    event: 'events',
     dateStartParamKey: 'date_gte',
     dateEndParamKey: 'date_lte',
     notRepeatParam: 'repeat=false',
